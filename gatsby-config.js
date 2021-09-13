af6e81de8a56b938ec0c0b5108efe7429bec34b4
kalynbeach <kalynbeach@gmail.com>
2021-09-12 23:46:29 -0700

    Update most npm packages, get prefix-paths working for IPFS static hosting

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,9 +5,11 @@ module.exports = {
     author: `@kalynbeach`,
     siteUrl: `https://kalyn.eth/`,
   },
+  pathPrefix: '__GATSBY_IPFS_PATH_PREFIX__',
   plugins: [
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-image`,
+    'gatsby-plugin-ipfs',
     `gatsby-plugin-sass`,
     {
       resolve: `gatsby-source-filesystem`,
