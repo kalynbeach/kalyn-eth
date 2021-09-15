eb5c728bd29eeee218a49d7233bdc17702f8adb2
kalynbeach <kalynbeach@gmail.com>
2021-09-14 23:44:25 -0700

    Clean up layout spacing, mobile styles

--- src/components/Nav/nav.js
+++ src/components/Nav/nav.js
@@ -13,12 +13,13 @@ const Nav = () => (
       <Link to="/contact/">Contact</Link>
     </div>
     <StaticImage
-        src="../images/CS-500-Gray.png"
-        width={100}
-        quality={100}
-        formats={["AUTO", "WEBP", "AVIF"]}
-        alt="Chromie Squiggle #500"
-      />
+      src="../../images/CS-500-Gray.png"
+      width={150}
+      quality={100}
+      formats={["AUTO", "WEBP", "AVIF"]}
+      alt="Chromie Squiggle #500"
+      className="squiggle"
+    />
   </aside>
 )
 
