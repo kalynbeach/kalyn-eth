8eb7040b3a823690d80a3022be33d484c25ac3c3
Kalyn Beach <kalynbeach@gmail.com>
2021-09-18 18:57:12 -0700

    Move page content to JSON, add gatsby-plugin-ipfs once more

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,52 +3,22 @@ import * as React from "react"
 
 import Layout from "../components/Layout/layout"
 import Seo from "../components/seo"
+import AboutJSON from "../../content/about.json"
 
 import "../styles/about.scss"
 
 const AboutPage = () => (
   <Layout>
     <Seo title="about" />
-    <h1>About</h1>
-    <strong>gm</strong>
+    <h1>{ AboutJSON.title }</h1>
+    <strong>{ AboutJSON.heading }</strong>
     <br/>
     <div className="about-statement">
-      <p>
-        My name's Kalyn Beach. 
-      </p>
-      <p>
-        I'm a musician + engineer living in Los Angeles, CA.
-      </p>
-      <p>
-        I love to listen and create.
-      </p>
-      <p>
-        I'm grateful to be here, alive; priviledged, living with
-        relative comfort in America, given the opportunities I've
-        needed to grow, compensated enough to thrive.
-      </p>
-      <p>
-        As a '90s kid who grew up on the Internet, it's been a 
-        trip watching how the world has played out over the
-        past couple decades. There's insane potential in what
-        we can do individually and collectively, yet so many
-        people are still systematically held back in life for
-        no good reason. 
-      </p>
-      <p>
-        Through art and tech, I aim to inspire and empower people
-        less fortunate than I - on the largest possible scale.
-        More diverse stories; more impactful experiences; more
-        human connection. More equity; more financial freedom;
-        more opportunity.
-      </p>
-      <p>
-        I'm driven to create and share art that resonates with people,
-        and to uplift artists who are driven to do the same.
-        With code, I intend to contribute to platforms and communities
-        that share these ideals for the future and are building toward
-        them today. Much to learn, much to give.
-      </p>
+      {
+        AboutJSON.body.map((p, index) => {
+          return <p key={`about_body_${index}`}>{ p.text }</p>
+        })
+      }
     </div>
     <div className="about-tech">
       <h2>Tech</h2>
@@ -73,6 +43,7 @@ const AboutPage = () => (
             <li>Web3.js</li>
             <li>p5.js</li>
             <li>{"-> p5.sound"}</li>
+            <li>IPFS</li>
           </ul>
         </div>
       </div>
--- src/pages/work.js
+++ src/pages/work.js
@@ -3,44 +3,27 @@ import * as React from "react"
 
 import Layout from "../components/Layout/layout"
 import Seo from "../components/seo"
+import WorkJSON from "../../content/work.json"
 
 import "../styles/work.scss"
 
 const WorkPage = () => (
   <Layout>
     <Seo title="work" />
-    <h1>Work</h1>
+    <h1>{ WorkJSON.title }</h1>
     <div className="work-grid">
-      <div className="work-position">
-        <span className="position-period">2014 - 2015</span>
-        <span className="position-title">Web Development Contractor</span>
-        <span className="position-company">CPR Web Contracting</span>
-        <p className="position-desc">
-          Built informational micro-sites, gained foundational web dev experience.
-        </p>
-      </div>
-      <div className="work-position">
-        <span className="position-period">06/2015 - 09/2015</span>
-        <span className="position-title">Software Engineer Intern</span>
-        <span className="position-company">Demand Media</span>
-        <p className="position-desc">
-          Interned on the eHow.com engineering team: implemented frontend
-          features, built internal utilities, started to learn about web
-          development at scale.
-        </p>
-      </div>
-      <div className="work-position">
-        <span className="position-period">09/2015 - Present</span>
-        <span className="position-title">Software Engineer</span>
-        <span className="position-company">Leaf Group (formerly Demand Media)</span>
-        <p className="position-desc">
-          Ongoing work on multiple large-scale, internal web content systems handling
-          dozens of sites and millions of impressions per month. Collaborated with a small
-          team (c.2015-2017) to build a central content platform frontend, still in use today.
-          More recently working on new sites, web performance analysis & improvement, and
-          internal workflow tools.
-        </p>
-      </div>
+      {
+        WorkJSON.history.map((position, index) => {
+          return (
+            <div className="work-position" key={`work_history_${index}`}>
+              <span className="position-period">{ position.period }</span>
+              <span className="position-title">{ position.title }</span>
+              <span className="position-company">{ position.company }</span>
+              <p className="position-desc">{ position.description }</p>
+            </div>
+          )
+        })
+      }
     </div>
   </Layout>
 )
